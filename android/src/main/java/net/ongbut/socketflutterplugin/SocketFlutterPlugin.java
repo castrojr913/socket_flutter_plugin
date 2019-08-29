2b2ca6f843103db406e2607e14a32f4606a9117d
Jesús Castro <castrojr913@gmail.com>
2019-08-29 15:43:17 -0500

    Fix UiThread exception

--- android/src/main/java/net/ongbut/socketflutterplugin/SocketFlutterPlugin.java
+++ android/src/main/java/net/ongbut/socketflutterplugin/SocketFlutterPlugin.java
@@ -1,5 +1,7 @@
 package net.ongbut.socketflutterplugin;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import java.net.URISyntaxException;
@@ -21,79 +23,85 @@ import org.json.JSONObject;
 
 /** SocketFlutterPlugin */
 public class SocketFlutterPlugin implements MethodCallHandler {
-  /** Plugin registration. */
+    /** Plugin registration. */
 
-  private Socket mSocket;
-  private MethodChannel channel;
+    private Socket mSocket;
+    private MethodChannel channel;
 
-  public SocketFlutterPlugin(MethodChannel channel) {
-    this.channel = channel;
-  }
-
-
-  /** Plugin registration. */
-  public static void registerWith(Registrar registrar) {
-    final MethodChannel channel = new MethodChannel(registrar.messenger(), "socket_flutter_plugin");
-    channel.setMethodCallHandler(new SocketFlutterPlugin(channel));
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("socket")) {
-        {
-          try {
-            String url = call.argument("url");
-            mSocket = IO.socket(url);
-            Log.d("SocketIO ","Socket initialised");
-          } catch (URISyntaxException e) {
-            Log.e("SocketIO ",e.toString());
-          }
-        }
-      result.success("created");
-
-
-    } else if (call.method.equals("connect")){
-        mSocket.connect();
-        Log.d("SocketIO  ","Connected");
-        result.success("connected");
-    } else if (call.method.equals("emit")){
-        String message = call.argument("message");
-        String topic = call.argument("topic");
-        Log.d("SocketIO  ","Pushing " +  message + " on topic " + topic);
-        JSONObject jb = null;
-        try {
-            jb = new JSONObject(message);
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        if(jb != null){
-            mSocket.emit(topic,jb);
-            result.success("sent");
-        }
-
-    } else if (call.method.equals("on")){
-        String topic = call.argument("topic");
-        Log.d("SocketIO  ","registering to "+ topic + " topic");
-        mSocket.on(topic, onNewMessage);
-        result.success("sent");
-    }else if(call.method.equals("unsubscribe")){
-        String topic = call.argument("topic");
-        mSocket.off(topic);
-    }else {
-        result.notImplemented();
-        Log.d("SocketIO ","Not Implemented");
+    public SocketFlutterPlugin(MethodChannel channel) {
+        this.channel = channel;
     }
-  }
 
-  private Emitter.Listener onNewMessage = new Emitter.Listener() {
+
+    /** Plugin registration. */
+    public static void registerWith(Registrar registrar) {
+        final MethodChannel channel = new MethodChannel(registrar.messenger(), "socket_flutter_plugin");
+        channel.setMethodCallHandler(new SocketFlutterPlugin(channel));
+    }
+
     @Override
-    public void call(final Object... args) {
-        Gson gson = new Gson();
-        String data = gson.toJson(args[0]);
-        Log.d("SocketIO ", "Received " + data);
-        Map<String, String> myMap= new HashMap<>();
-        myMap.put("message", data);
-        channel.invokeMethod("received", myMap);
+    public void onMethodCall(MethodCall call, Result result) {
+        if (call.method.equals("socket")) {
+            {
+                try {
+                    String url = call.argument("url");
+                    mSocket = IO.socket(url);
+                    Log.d("SocketIO ","Socket initialised");
+                } catch (URISyntaxException e) {
+                    Log.e("SocketIO ",e.toString());
+                }
+            }
+            result.success("created");
+
+
+        } else if (call.method.equals("connect")){
+            mSocket.connect();
+            Log.d("SocketIO  ","Connected");
+            result.success("connected");
+        } else if (call.method.equals("emit")){
+            String message = call.argument("message");
+            String topic = call.argument("topic");
+            Log.d("SocketIO  ","Pushing " +  message + " on topic " + topic);
+            JSONObject jb = null;
+            try {
+                jb = new JSONObject(message);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            if(jb != null){
+                mSocket.emit(topic,jb);
+                result.success("sent");
+            }
+
+        } else if (call.method.equals("on")){
+            String topic = call.argument("topic");
+            Log.d("SocketIO  ","registering to "+ topic + " topic");
+            mSocket.on(topic, onNewMessage);
+            result.success("sent");
+        }else if(call.method.equals("unsubscribe")){
+            String topic = call.argument("topic");
+            mSocket.off(topic);
+        }else {
+            result.notImplemented();
+            Log.d("SocketIO ","Not Implemented");
+        }
     }
-  };
+
+    private Emitter.Listener onNewMessage = new Emitter.Listener() {
+        @Override
+        public void call(final Object... args) {
+            Gson gson = new Gson();
+            final String data = gson.toJson(args[0]);
+            Log.d("SocketIO ", "Received " + data);
+            Handler handler = new Handler(Looper.getMainLooper());
+            handler.post(new Runnable() {
+                @Override
+                public void run() {
+                    channel.invokeMethod("received", new HashMap<String, String>(){{
+                        put("message", data);
+                    }});
+                }
+            });
+        }
+    };
 }
